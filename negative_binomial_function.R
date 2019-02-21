07408809a18c1a6411bf57370f5207e7310b9f86
Bill White <bill.c.white@gmail.com>
2019-02-21 16:33:36 -0600

    Added negative binomial function.

---
+++ negative_binomial_function.R
@@ -0,0 +1,18 @@
+# negative_binomial_function.R - Bill White - 2/21/19
+# https://en.wikipedia.org/wiki/Negative_binomial_distribution
+# k is the numeric of support for the function (number of successes)
+# r is the numeric vector of trials r of successes
+# p is the numeric vector of probabilities of success [0, 1]
+amstat_negative_binomial <- function(k, r, p) {
+  results <- lapply(k, function(x) {
+    data.frame(x = x, 
+               y = choose(r + x - 1, x) * ((1 - p) ^ r) * (p ^ x),
+               r = as.factor(r),
+               p = as.factor(p),
+               grp = paste(as.factor(r), as.factor(p)))
+  })
+  return_results <- do.call(rbind, results)
+  print(return_results)
+  #cat("negative binomial mean (params: k =", k, "= p * r / 1 - p", (p * r) / (1 - p), "\n")
+  return_results
+}
---
+++ negative_binomial_function_tests.R
@@ -0,0 +1,21 @@
+# negative_binomial_function_tests.R - Bill White - 2/21/19
+# https://en.wikipedia.org/wiki/Negative_binomial_distribution
+
+library(ggplot2)
+
+source("negative_binomial_function.R")
+
+# k is the numeric of support for the function (number of successes)
+# r is the numeric vector of trials r of successes
+# p is the numeric vector of probabilities of success [0, 1]
+k_s <- seq.int(from = 1, to = 25, by = 1)
+r_s <- c(1, 2, 3, 4, 5 ,10, 20, 40)
+#p_s <- c(0.1, 0.25, 0.33, 0.5, 0.75, 0.875)
+p_s <- c(0.5)
+plot_data <- amstat_negative_binomial(k = k_s, r = r_s, p = p_s)
+head(plot_data)
+plot_obj <- ggplot(plot_data, aes(x = x, y = y, colour = r)) + 
+  geom_point() + geom_line() + xlab("k") + ylab("PMF") +
+  ggtitle(label = "Negative Binomial Distribution", 
+          subtitle = "Probability Mass Function")
+print(plot_obj)

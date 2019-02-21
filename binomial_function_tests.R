cf12485020a6280639a939a1c7f8a82d8c0f4928
Bill White <bill.c.white@gmail.com>
2019-02-21 16:35:20 -0600

    Fixed cut-and-paste error.

--- binomial_function_tests.R
+++ binomial_function_tests.R
@@ -1,13 +1,13 @@
 # binomial_function_tests.R - Bill White - 2/18/19
 # https://en.wikipedia.org/wiki/Binomial_distribution
+# k is the number of observed successes (support)
+# n is the number of draws (i.e. quantity drawn in each trial) { 0, 1, 2, ... N}
+# p is the numeric vector of probabilities of success [0, 1]
 
 library(ggplot2)
 
 source("binomial_function.R")
 
-# k is the number of observed successes (support)
-# n is the number of draws (i.e. quantity drawn in each trial) { 0, 1, 2, ... N}
-# p is the numeric vector of probabilities of success [0, 1]
 k_s <- seq.int(from = 0, to = 40, by = 1)
 n_s <- c(20, 20, 40)
 p_s <- c(0.5, 0.7, 0.5)

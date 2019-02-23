d7805fc4de9f7cd4feb77968709357cc9549450f
Bill White <bill.c.white@gmail.com>
2019-02-22 23:53:25 -0600

    Implemented Wolfram version of Weibull discrete distribution. Looks much better than the Wikipedia version.

--- weibull_function.R
+++ weibull_function.R
@@ -10,8 +10,7 @@
 # x_values - numeric vector of x vaklues to evalute
 # alphas - scale - numeric vector greater tha zero
 # betas - shape - numeric vector greater tha zero
-# http://mathworld.wolfram.com/WeibullDistribution.html
-amstat_discrete_weibull <- function(x_values, alphas = 1, betas = 0, mu = 0) {
+amstat_discrete_weibull_first <- function(x_values, alphas = 1, betas = 0, mu = 0) {
   x_results <- lapply(x_values, function(x) {
     a_results <- lapply(alphas, function(a) {
       b_results <- lapply(betas, function(b) {
@@ -30,3 +29,31 @@ amstat_discrete_weibull <- function(x_values, alphas = 1, betas = 0, mu = 0) {
   })
   do.call(rbind, x_results)
 }
+
+# http://mathworld.wolfram.com/WeibullDistribution.html
+amstat_discrete_weibull_wolfram <- function(x_values, 
+                                            alphas = 1, 
+                                            betas = 0, 
+                                            mu = 0) {
+  x_results <- lapply(x_values, function(x) {
+    a_results <- lapply(alphas, function(a) {
+      b_results <- lapply(betas, function(b) {
+        if (x <= mu) {
+          f_x_alpha_beta <- 0
+        } else {
+          # x > mu
+          f_x_alpha_beta <- 
+            ((x - mu) ^ (a - 1)) * 
+            (exp(-((x - mu) / b) ^ a))
+        }
+        data.frame(x = x,
+                   y = f_x_alpha_beta,
+                   Parameters = paste("a =", a, ", b = ", b, "u =", mu))
+      })
+      do.call(rbind, b_results)
+    })
+    do.call(rbind, a_results)
+  })
+  do.call(rbind, x_results)
+}
+
--- weibull_function_tests.R
+++ weibull_function_tests.R
@@ -10,14 +10,17 @@ library(ggplot2)
 
 source("weibull_function.R")
 
-a_s <- c(3)
-b_s <- c(2)
-u_s <- c(-1.5, 1, 2)
-x_s <- seq(from = max(u_s), to = 6, by = 0.1)
-plot_data <- amstat_discrete_weibull(x_values = x_s, 
-                                     alphas = a_s, 
-                                     betas = b_s, 
-                                     mu = u_s)
+# positive real numbers
+a_s <- c(1, 2)
+b_s <- c(1, 2)
+# any real numbers
+# u_s <- c(-1.5, 1, 2)
+u_s <- c(0)
+x_s <- seq(from = u_s[1], to = 6, by = 0.1)
+plot_data <- amstat_discrete_weibull_wolfram(x_values = x_s, 
+                                             alphas = a_s, 
+                                             betas = b_s, 
+                                             mu = u_s)
 head(plot_data, n = 10)
 plot_obj <- ggplot(plot_data, aes(x = x, y = y, colour = Parameters)) + 
   geom_point() + geom_line() + xlab("x") + ylab("PMF") +

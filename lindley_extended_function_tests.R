6e34d7b38dd82b4e1f1f5f9d564fad03f6a8dba4
bwhite <bill.c.white@gmail.com>
2019-03-05 17:48:42 -0600

    Minor graph adjustments,

--- lindley_extended_function_tests.R
+++ lindley_extended_function_tests.R
@@ -5,7 +5,7 @@ library(ggplot2)
 
 source("lindley_extended_function.R")
 
-x_s <- seq(from = 0, to = 5, by = 0.1)
+x_s <- seq(from = 0.1, to = 5, by = 0.1)
 a_s <- c(0.4, -1.8, 0.2, -3.5, 1.0)
 b_s <- c(1.2, 0.5, 1.2, 0.2, 1.0)
 l_s <- c(1, 0.9, 1.2, 0.6, 1.0)
@@ -18,7 +18,7 @@ plot_obj <- ggplot(raw_results, aes(x = x,
                                     y = y, 
                                     colour = Parameters)) + 
   geom_line() + 
-  xlab("x") + ylab("PMF") + 
+  xlab("x") + ylab("PMF") + ylim(0, 0.7) +
   ggtitle(label = "Extended Lindley Distribution",
           subtitle = "Probability Mass Function") +
   scale_fill_discrete(name = "Parameters")

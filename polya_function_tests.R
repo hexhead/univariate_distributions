86143dfdf9cedd52ca3eda5c9446ac7438a7b496
Bill White <bill.c.white@gmail.com>
2019-03-06 14:10:49 -0600

    Finished Polya implementation.

--- polya_function_tests.R
+++ polya_function_tests.R
@@ -1,32 +1,27 @@
 # polya_function_tests.R - Bill White - 2/26/19
 # 
-# http://www.math.wm.edu/~leemis/chart/UDR/PDFs/Polya.pdf
-#
-# Polya distribution (from http://www.math.wm.edu/˜leemis/chart/UDR/UDR.html)
-# The shorthand X ∼ Polya(n, p, β) is used to indicate that the random variable
-# X has the Polya distribution with parameters n, p, and β. A Polya random variable X with
-# parameters n, p, and β
+# https://www.encyclopediaofmath.org/index.php/P%C3%B3lya_distribution
+## See also: negative binomial and exponential distributions.
 # 
 # Parameters:
-# x_s - vector of numerics to evaluate (x values) - real > 0
+# k_s - vector of numerics to evaluate (x values) - 0 <= k <= n
 # n_s - vector of integer n's for range of x-axis 1.2...
 # p_s = vector of numeric - 0 < p_s < 1 
-# b_s - vector of numeric > 0
+# g_s - vector of numeric  gamma > 0
 
 library(ggplot2)
 
 source("polya_function.R")
 
-n <- 12
-x_s <- seq(from = 0, to = n, by = 1)
+n <- 6
 n_s <- seq(from = 1, to = n, by = 1)
 p_s <- c(0.1, 0.25, 0.5, 0.9)
-b_s <- c(2, 2, 2, 2)
-raw_results <- amstat_polya(x_s = x_s, p_s = p_s, b_s = b_s, n_s = n_s)
+g_s <- c(3, 3, 3, 3)
+raw_results <- amstat_polya(n_s = n_s, p_s = p_s, g_s = g_s)
 head(raw_results)
 plot_obj <- ggplot(raw_results, aes(x = x, y = y, colour = Parameters)) + 
-  geom_line() + 
-  xlab("x") + ylab("PDF") +
+  geom_point() + geom_line() +
+  xlab("k") + ylab("PDF") +
   ggtitle(label = "Polya Distribution", 
           subtitle = "Probability Mass Function") +
   scale_fill_discrete(name = "Parameters") +

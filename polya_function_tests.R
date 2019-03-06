4079697e3c3c3a8ee69479281cde6f568ccf5e07
Bill White <bill.c.white@gmail.com>
2019-03-06 17:29:48 -0600

    Cleaned up documentation.

--- polya_function_tests.R
+++ polya_function_tests.R
@@ -1,19 +1,19 @@
 # polya_function_tests.R - Bill White - 2/26/19
 # 
-# https://www.encyclopediaofmath.org/index.php/P%C3%B3lya_distribution
-## See also: negative binomial and exponential distributions.
-# 
-# Parameters:
-# k_s - vector of numerics to evaluate (x values) - 0 <= k <= n
-# n_s - vector of integer n's for range of x-axis 1.2...
-# p_s = vector of numeric - 0 < p_s < 1 
-# g_s - vector of numeric  gamma > 0
+# Test the Polya implementations by comparing ggplot to web site references.
 
 library(ggplot2)
 
 source("polya_function.R")
 
-n <- 6
+# -----------------------------------------------------------------------------
+# https://www.encyclopediaofmath.org/index.php/P%C3%B3lya_distribution
+# See also: negative binomial and exponential distributions.
+# Parameters:
+# k_s - vector of numerics to evaluate (x values) - 0 <= k <= n
+# n_s - vector of integer n's for range of x-axis 1.2...
+# p_s = vector of numeric - 0 < p_s < 1 
+# g_s - vector of numeric  gamma > 0
 n_s <- c(5, 10, 15, 20)
 p_s <- c(0.05, 0.1, 0.25, 0.5)
 g_s <- rep(0.1, length(p_s))
@@ -30,6 +30,11 @@ print(plot_obj)
 
 # -----------------------------------------------------------------------------
 # https://www.vosesoftware.com/riskwiki/Polyadistribution.php
+# Poisson(Gamma(a,b)) = Pólya(a, b)
+# Parameters:
+# x_s - vector of numerics to evaluate (x values) - 0,1,2...
+# a_s - vector of numeric - alpha > 0
+# b_s = vector of numeric - beta > 0
 x_s <- seq(from = 0, to = 30, by = 1)
 a_s <- c(0.5, 1.5, 6.6)
 b_s <- c(22, 10, 2.5)

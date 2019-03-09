f4ae0b5cd48e80b01c1834dbfe46f1f5509fae60
Bill White <bill.c.white@gmail.com>
2019-03-09 00:30:32 -0600

    Added Pearson Type IX distribution.

---
+++ pearson_type_ix_function.R
@@ -0,0 +1,18 @@
+# pearson_type_ix_function.R - Bill White - 3/8/19
+# 
+# https://www.statisticshowto.datasciencecentral.com/pearson-distribution/
+# 
+# Parameters:
+# x_s - vector of numerics to evaluate (x values) - 0 <= x <= 1
+# m_s - vector of numerics - m
+
+amstat_pearson_type_ix <- function(x_s, m_s) {
+  x_results <- lapply(x_s, function(x) {
+    m_results <- lapply(m_s, function(m) {
+      y <-  (1 + x) ^ m
+      data.frame(x = x, y = y, Parameters = sprintf("m=%4.2f", m))
+    })         
+    do.call(rbind, m_results)
+  })
+  do.call(rbind, x_results)
+}
---
+++ pearson_type_ix_function_tests.R
@@ -0,0 +1,23 @@
+# pearson_type_ix_function_tests.R - Bill White - 3/8/19
+# 
+# https://www.statisticshowto.datasciencecentral.com/pearson-distribution/
+# 
+# Parameters:
+# x_s - vector of numerics to evaluate (x values) - 0 <= x <= 1
+# m_s - vector of numerics - m
+
+library(ggplot2)
+
+source("pearson_type_ix_function.R")
+
+x_s <- seq(from = 0, to = 1, by = 0.1)
+m_s <- c(2, 3, 4)
+raw_results <- amstat_pearson_type_ix(x_s = x_s, m_s = m_s)
+head(raw_results)
+plot_obj <- ggplot(raw_results, aes(x = x, 
+                                    y = y, 
+                                    colour = Parameters)) + 
+  geom_line() +   xlab("x") + ylab("PDF") + 
+  ggtitle(label = "Pearson Type IX", subtitle = "Probability Density Function") +
+  scale_fill_discrete(name = "Parameters")
+print(plot_obj)

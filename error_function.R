878178b0750c5e307cd7aaff2c7ae6fa4d748359
Bill White <bill.c.white@gmail.com>
2019-03-12 16:08:37 -0500

    More attempts at erf(x) implementations. Sticking with generalized error functions E_n(x) with 'n' selecting the specific curve, 1 = linear, 2 = erf(x), ... others uncharacterized.

--- error_function.R
+++ error_function.R
@@ -5,24 +5,46 @@
 # probability, statistics, and partial differential equations describing 
 # diffusion. 
 
+source("gamma_function.R")
 source("gamma_incomplete_function.R")
 
-amstat_error <- function(x_s) {
-  x_results <- lapply(x_s, function(x) {
-    nearinf <- 100
-    t1 <- sqrt(pi / 2)
-    t2 <- exp((x ^ 2) / 2)
-    j_results <- lapply(1:nearinf, function(j) {
-      (2 ^ j * factorial(j)) / 
-        (factorial(2 * j + 1)) * x ^ (2 * j)
+# -----------------------------------------------------------------------------
+# for x_s > 0
+# https://ipfs.io/ipfs/QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/wiki/Error_function.html
+# # Parameters:
+# x_s - vector of numerics x-axis values  x = [0, inf) 
+# n_s - vector of numerics - selects generalized error function E_n(x)
+amstat_error_en_x <- function(x_s, n_s) {
+  n_results <- lapply(n_s, function(n) {
+    x_results <- lapply(x_s, function(x) {
+      t1 <- 1 / sqrt(pi)
+      t2 <- as.numeric(amstat_gamma(n))
+      one_over_n <- 1 / n
+      g_1_n <- amstat_gamma(one_over_n)
+      g_i_n_x <- as.numeric(amstat_gamma_incomplete(n_s = one_over_n, x_s = x ^ n)$y)
+      t3 <- g_1_n - g_i_n_x
+      y <- t1 * t2 * t3
+      data.frame(x = x, y = y, Parameters = sprintf("n=%2.0f", n))
     })
-    y <- (t1 * t2) - (x * sum(unlist(j_results)))
+    do.call(rbind, x_results)
+  })
+  do.call(rbind, n_results)
+}
+
+# -----------------------------------------------------------------------------
+# for x_s > 0
+# https://ipfs.io/ipfs/QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/wiki/Error_functio.html
+amstat_error_erf_x <- function(x_s) {
+  x_results <- lapply(x_s, function(x) {
+    one_over_sqrt_pi <- 1 / sqrt(pi)
+    g_i <- amstat_gamma_incomplete(1 / 2, x ^ 2)$y
+    y <- 1 - (one_over_sqrt_pi * g_i)
     data.frame(x = x, y = y, Parameters = "")
-    
   })
   do.call(rbind, x_results)
 }
 
+# -----------------------------------------------------------------------------
 amstat_error_stackexchange <- function(a, b, z, maxt = 5) {
   s <- 1:maxt
   a <- a + s
@@ -37,7 +59,8 @@ amstat_error_stackexchange <- function(a, b, z, maxt = 5) {
   data.frame(x = z, y = ss, Parameters = "")
 }
 
-amstat_error <- function(x_s) {
+# -----------------------------------------------------------------------------
+amstat_error1 <- function(x_s) {
   x_results <- lapply(x_s, function(x) {
     nearinf <- 100
     t1 <- sqrt(pi / 2)
@@ -53,13 +76,39 @@ amstat_error <- function(x_s) {
   do.call(rbind, x_results)
 }
 
+# -----------------------------------------------------------------------------
 # https://ipfs.io/ipfs/QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/wiki/Error_function.html
-amstat_error_generalized <- function(x_s) {
-  x_results <- lapply(x_s, function(x) {
-    t1 <- 1 / sqrt(pi)
-    t2 <- amstat_gamma_incomplete(n_s = 1, x_s = x ^ 2)$y
-    y <- t1 * t2
-    data.frame(x = x, y = y, Parameters = "")
-  }) 
-  do.call(rbind, x_results)
+# Notable cases are:
+#
+# E0(x) is a straight line through the origin:
+# \textstyle E_{0}(x)={\frac {x}{e{\sqrt {\pi }}}}
+# E2(x) is the error function, erf(x).
+amstat_error_generalized <- function(x_s, n_s) {
+  n_results <- lapply(n_s, function(n) {
+    x_results <- lapply(x_s, function(x) {
+      infinity <- 100
+      t1 <- factorial(n) / sqrt(pi)
+      t2 <- sum(unlist(lapply(0:infinity, function(p) {
+          ((-1) ^ p) * (x ^ (n * p + 1)) / ((n * p + 1) * factorial(p))
+      })))
+      y <- t1 * t2
+      data.frame(x = x, y = y, Parameters = sprintf("n = %5.2f", n))
+    }) 
+    do.call(rbind, x_results)
+  })
+  do.call(rbind, n_results)
+}
+
+# Abramowitz and Stegun 7.1.6
+amstat_error_as <- function(z_s) {
+  infinity <- 100
+  z_results <- lapply(z_s, function(z) {
+    sum_term <- sum(unlist(lapply(0:infinity, function(n) {
+      divisor <- prod(unlist(lapply(0:n, function(i) 2 * i + 1)))
+      ((2 ^ n) / divisor) * z ^ (2 * n + 1)
+    })))
+    y <- (2 / sqrt(pi)) * exp(-(z ^ 2)) * sum_term
+    data.frame(x = z, y = y, Parameters = "")
+  })
+  do.call(rbind, z_results)
 }
--- error_function_tests.R
+++ error_function_tests.R
@@ -2,19 +2,31 @@
 
 # Parameters:
 # x_s - vector of numerics x-axis values  x = [0, inf) 
+# n_s - vector of numerics - selects generalized error function E_n(x)
 
 library(ggplot2)
 
 source("error_function.R")
 
-x_s <- seq(from = 0, to = 3, by = 0.1)
-raw_results <- amstat_error_generalized(x_s = x_s)
+# other attempts:
+# Abramowitz and Stegun
+#raw_results <- amstat_error_as(z_s = x_s)
+# x_s <- seq(from = -3, to = 3, by = 0.1)
+#n_s <- 1:4
+#
+#raw_results <- amstat_error_en_x(x_s = x_s, n_s = n_s)
+
+#raw_results <- amstat_error_erf_x(x_s = x_s)
+
+# https://ipfs.io/ipfs/QmXoypizjW3WknFiJnKLwHCnL72vedxjQkDDP1mXWo6uco/wiki/Error_function.html
+x_s <- seq(from = -3, to = 3, by = 0.05)
+raw_results <- amstat_error_generalized(x_s = x_s, n_s = c(1:4, 4.25))
 head(raw_results)
 plot_obj <- ggplot(raw_results, aes(x = x, y = y, colour = Parameters)) + 
   geom_line() +
-  xlab("x") + ylab("erf(x)") +
-  ggtitle(label = "Error (erf) distribution", 
-          subtitle = "Probability Density Function") +
+  xlab("x") + ylab("E_n(x)") + ylim(-3, 3) +
+  ggtitle(label = "Generalized Error E_n(x) functions", 
+          subtitle = "1 = straight line, 2 = erf(x),... other not characterized") +
   scale_fill_discrete(name = "Parameters")
 print(plot_obj)
 

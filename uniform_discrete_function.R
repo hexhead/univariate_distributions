f0280a8323f05a8a45c8231d0df13cb0ad83c068
Bill White <bill.c.white@gmail.com>
2019-03-13 13:36:08 -0500

    Renamed 'uniform' to 'uniform_discrete'.

--- uniform_discrete_function.R
+++ uniform_discrete_function.R
@@ -1,10 +1,10 @@
-# uniform_function.R - Bill White - 2/21/19
+# uniform_discrete_function.R - Bill White - 2/21/19
 # 
-# https://www.wikiwand.com/en/Discrete_uniform_distribution
+# https://www.wikiwand.com/en/Discrete_uniform_discrete_distribution
 # symmetric probability distribution whereby a finite number of values are equally 
 # likely to be observed; every one of n values has equal probability 1/n.
 # Included for completeness.
-amstat_uniform <- function(a, b) {
+amstat_uniform_discrete <- function(a, b) {
   if (b < a) stop(paste("b", b, "must be greater than a", a))
   n <- b - a + 1
   data.frame(x = a:b, y = 1 / n, Parameters = paste("a =", a, "b =", b, "n =", n))
--- uniform_discrete_function_tests.R
+++ uniform_discrete_function_tests.R
@@ -1,18 +1,22 @@
-# uniform_function_tests.R - Bill White - 2/18/19
+# uniform_discrete_function_tests.R - Bill White - 2/18/19
 # https://en.wikipedia.org/wiki/Discrete_uniform_distribution
 
 library(ggplot2)
 
-source("uniform_function.R")
+source("uniform_discrete_function.R")
 
-raw_results <- amstat_uniform(2, 4)
+a <- 2
+b <- 4
+raw_results <- amstat_uniform_discrete(a, b)
 
 plot_data <- raw_results
 head(plot_data)
 plot_obj <- ggplot(plot_data, aes(x = x, y = y)) + 
   geom_pointrange(size = 1, shape = 22, ymin = 0, ymax = plot_data$y) + 
   xlab("k") + ylab(expression("1 / n")) +
-  ggtitle(label = "Uniform Distribution", 
+  ggtitle(label = 
+            sprintf("Uniform Discrete Distribution, Parameters: a = %2.0f,  b = %2.0f", 
+                    a, b), 
           subtitle = "Probability Mass Function") +
   scale_fill_discrete(name = "Parameters")
 print(plot_obj)
